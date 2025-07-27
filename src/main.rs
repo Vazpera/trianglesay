6b88b69f7385ec6cb8dd962472c07f6ac236c20f
Vazpera <jthoesel10@hotmail.com>
2025-07-27 06:42:56 -0700

    Cleaned up pull request

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     let default_word: Vec<String> = vec!["no freaking way".to_string()];
     let say_art: String = "     \\  /\\\n      \\/  \\\n      /*-* \\\n      ------\n".to_string();
 
-    let words: Vec<String> = if args.say.is_some() {
-        args.say
-            .unwrap_or(default_word)
+    let words: Vec<String> = match (args.say, args.weather) {
+        (Some(j), _) => j
             .iter()
             .flat_map(|s| s.split('\n').map(String::from).collect::<Vec<_>>())
-            .collect()
-    } else if args.weather {
-        let response = reqwest::blocking::get("https://wttr.in/?format=\"%C %t in %l\"")?.text()?;
-        response
-            .lines()
-            .map(String::from)
-            .collect()
-    } else {
-        default_word.clone()
+            .collect(),
+        (_, true) => {
+            let response =
+                reqwest::blocking::get("https://wttr.in/?format=\"%C %t in %l\"")?.text()?;
+            response.trim_matches('\"').lines().map(|x| x.to_owned()).collect::<Vec<String>>()
+        }
+        (_, _) => default_word.clone(),
     };
 
-    let len = words
-        .iter()
-        .map(|s| s.len())
-        .max()
-        .unwrap_or(0);
+    let len = words.iter().map(|s| s.len()).max().unwrap_or(0);
 
     let bars = std::iter::repeat('-').take(len + 2).collect::<String>();
 
